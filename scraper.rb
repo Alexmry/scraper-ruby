bd22289a0b2e5be24f7de2dc2bcfc72a6c867757
Alexandre Morey <alexander006_9@hotmail.com>
2022-01-17 15:29:47 +0000

    scraper method

--- scraper.rb
+++ scraper.rb
@@ -6,6 +6,16 @@ def scraper
     url = "https://techcrunch.com/"
     unparsed_page = HTTParty.get(url)
     parsed_page = Nokogiri::HTML(unparsed_page)
+    headers= parsed_page.css('a.post-block__title__link')
+
+    headers.each do |header|
+        articles = {
+            title: header.children.text,
+            link: header.attributes["href"].value
+        }
+
+    end
+
     byebug
 end
 
