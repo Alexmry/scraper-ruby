36eb82d3378d7407f5f786e73f0d0334e5361912
Alexandre Morey <alexander006_9@hotmail.com>
2022-01-17 15:42:13 +0000

    scraper is working

--- scraper.rb
+++ scraper.rb
@@ -6,16 +6,16 @@ def scraper
     url = "https://techcrunch.com/"
     unparsed_page = HTTParty.get(url)
     parsed_page = Nokogiri::HTML(unparsed_page)
+    articles = Array.new
     headers= parsed_page.css('a.post-block__title__link')
 
     headers.each do |header|
-        articles = {
+        article = {
             title: header.children.text,
             link: header.attributes["href"].value
         }
-
+        articles << article
     end
-
     byebug
 end
 
